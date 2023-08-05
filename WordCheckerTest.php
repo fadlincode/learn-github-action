ef97c427ff096ff5929ee2ef4835da7a74473f70
fadlincode <fadlinarsin12@gmail.com>
2023-08-05 10:12:22 +0800

    add Unit Test

---
+++ WordCheckerTest.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'word_checker.php';
+
+use PHPUnit\Framework\TestCase;
+
+class WordCheckerTest extends TestCase
+{
+    public function testSanitizeText()
+    {
+        $inputText = "This is a badword1 sentence with badword2 words.";
+        $expectedCleanText = "this is a ******** sentence with ******** words.";
+
+        $cleanText = sanitizeText($inputText);
+
+        $this->assertEquals($expectedCleanText, $cleanText);
+    }
+
+    public function testSanitizeTextWithNoBadWords()
+    {
+        $inputText = "This is a clean sentence without bad words.";
+        $expectedCleanText = "this is a clean sentence without bad words.";
+
+        $cleanText = sanitizeText($inputText);
+
+        $this->assertEquals($expectedCleanText, $cleanText);
+    }
+}
+
+?>
